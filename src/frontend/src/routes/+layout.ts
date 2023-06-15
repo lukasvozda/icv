e90eec9e09a4ffc58f5f6ee7efc209422ffbc339
Lukáš Vožda <vozdalukas@gmail.com>
2023-06-15 20:23:02 +0200

    added skeleton UI

--- src/frontend/src/routes/+layout.ts
+++ src/frontend/src/routes/+layout.ts
@@ -1,1 +1,10 @@
 export const prerender = true;
+
+// Your selected Skeleton theme:
+import '@skeletonlabs/skeleton/themes/theme-modern.css';
+
+// This contains the bulk of Skeletons required styles:
+import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+import '../app.postcss';
--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
 	import { createActor } from '../../../declarations/backend';
 
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+
+	import { Toast, toastStore } from '@skeletonlabs/skeleton';
+	import type { ToastSettings } from '@skeletonlabs/skeleton';
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+
 	let input = '';
 	let disabled = false;
 	let greeting = '';
 
 	const handleOnSubmit = async () => {
 		disabled = true;
+		console.log('submit');
 
 		try {
 			// Canister IDs are automatically expanded to .env config - see vite.config.ts
@@ -20,6 +27,12 @@
 
 			// Call the IC
 			greeting = await actor.greet(input);
+
+			const t: ToastSettings = {
+				message: greeting,
+				timeout: 10000
+			};
+			toastStore.trigger(t);
 		} catch (err: unknown) {
 			console.error(err);
 		}
@@ -29,22 +42,77 @@
 </script>
 
 <main>
+	<LightSwitch />
+
+	<h1 class="h1">Skeleton H1</h1>
+	<h2 class="h2">Skeleton H2</h2>
+	<h3 class="h3">Skeleton H3</h3>
+	<h4 class="h4">Skeleton H4</h4>
+	<h5 class="h5">Skeleton H5</h5>
+	<h6 class="h6">Skeleton H6</h6>
+
+	<h1 class="h1">
+		<span
+			class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+			>Design.</span
+		>
+	</h1>
+	<h1 class="h1">
+		<span
+			class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone"
+			>Build.</span
+		>
+	</h1>
+	<h1 class="h1">
+		<span
+			class="bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone"
+			>Deploy.</span
+		>
+	</h1>
+
+	<blockquote class="blockquote">
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia atque consectetur saepe
+		accusantium quam odio earum assumenda quisquam nesciunt voluptates consequuntur temporibus
+		cupiditate id neque quo, suscipit recusandae laboriosam et.
+	</blockquote>
+
 	<img src="logo2.svg" alt="DFINITY logo" />
 	<br />
 	<br />
-
 	<form on:submit|preventDefault={handleOnSubmit}>
-		<label for="name">Enter your name: &nbsp;</label>
-		<input id="name" alt="Name" type="text" bind:value={input} {disabled} />
-		<button type="submit">Click Me!</button>
-	</form>
+		<label for="name">Enter your name:</label>
+		<input
+			id="name"
+			alt="Name"
+			class="input"
+			type="text"
+			placeholder="Input"
+			bind:value={input}
+			{disabled}
+		/>
 
+		<button type="submit" class="btn btn-lg variant-filled" {disabled}>Button</button>
+	</form>
+	{#if disabled}
+		<ProgressRadial ... stroke={240} meter="stroke-primary-500" track="stroke-primary-500/30" />
+	{/if}
 	<section id="greeting">
 		{greeting}
 	</section>
+
+	<Toast />
 </main>
 
 <style lang="scss">
+	:root {
+		--theme-font-family-base: 'Quicksand', sans-serif;
+		--theme-font-family-heading: 'Quicksand', sans-serif;
+		/* ... */
+	}
+
+	h1 {
+		font-family: 'Quicksand', sans-serif;
+	}
 	img {
 		max-width: 50vw;
 		max-height: 25vw;
@@ -60,14 +128,7 @@
 		max-width: 40vw;
 		margin: auto;
 		align-items: baseline;
-		font-family: sans-serif;
 		font-size: 1.5rem;
-	}
-
-	button[type='submit'] {
-		padding: 5px 20px;
-		margin: 10px auto;
-		float: right;
 	}
 
 	#greeting {
